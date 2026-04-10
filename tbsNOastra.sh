62b4f08c6bb0fe401cb0395b4469b557f5c6a71f
stoopKid <48957485+defexnicolas@users.noreply.github.com>
2026-04-10 13:53:15 -0700

    Refactor tbsNOastra.sh for improved installation process

--- tbsNOastra.sh
+++ tbsNOastra.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
+set -e
 
-sudo apt-get update
-sudo apt-get full-upgrade
-sudo apt-get install module-assistant
-sudo module-assistant
-sudo apt-get install libproc-processtable-perl
-mkdir tbsdrivers/
-cd tbsdrivers/
+echo "Actualizando repositorios"
+apt-get update
+
+echo "Instalando dependencias"
+apt-get install -y git wget tar bzip2 gcc g++ make patchutils build-essential libproc-processtable-perl linux-headers-$(uname -r)
+
+echo "Creando carpeta de trabajo"
+mkdir -p /root/tbsdrivers
+cd /root/tbsdrivers
+
+echo "Eliminando compilaciones anteriores"
+rm -rf media_build media
+
+echo "Clonando repositorios TBS"
 git clone https://github.com/tbsdtv/media_build.git
-git clone --depth=1 https://github.com/tbsdtv/linux_media.git -b latest ./media
-cd media_build/
-sudo apt-get install linux-headers-generic
-sudo apt-get install patchutils
-sudo apt-get install libproc-processtable-perl
-sudo apt-get install gcc
-make dir DIR=../media
-make allyesconfig
-make -j4
-sudo make install
-wget http://www.tbsdtv.com/download/document/linux/tbs-tuner-firmwares_v1.0.tar.bz2
-sudo tar jxvf tbs-tuner-firmwares_v1.0.tar.bz2 -C /lib/firmware/
+git clone --depth=1 -b latest https://github.com/tbsdtv/linux_media.git media
 
-echo Instalacion Exitosa
+echo "Entrando a media_build"
+cd media_build
+
+echo "Preparando fuentes"
+make dir DIR=../media
+
+echo "Generando configuracion"
+make allyesconfig
+
+echo "Compilando"
+make -j$(nproc)
+
+echo "Instalando modulos"
+make install
+
+echo "Descargando firmware"
+cd /root/tbsdrivers
+wget -O tbs-tuner-firmwares_v1.0.tar.bz2 https://www.tbsdtv.com/download/document/linux/tbs-tuner-firmwares_v1.0.tar.bz2
+
+echo "Instalando firmware"
+tar -xjf tbs-tuner-firmwares_v1.0.tar.bz2 -C /lib/firmware/
+
+echo "Actualizando modulos"
+depmod -a
+
+echo "Proceso terminado"
+echo "Ahora reinicia con: reboot"
